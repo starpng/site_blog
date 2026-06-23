f08de6211c588511f0df1cccfbf61cc7e5d6ea2a
Soliloquy / ink <isopod2024@protonmail.com>
2026-06-23 00:05:11 -0700

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Re: The soliloquy",
+	url: "https://inksoliloquy.neocities.org/blog",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "A place for my yapping",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "ink / ink&",
+		email: "",
+		url: "https://inksoliloquy.neocities.org/"
 	}
 }
